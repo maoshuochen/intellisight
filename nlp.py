586f7adad3dfea4955ae4b27dcb089a6087adf89
maoshuochen <maoshuochen@gmail.com>
2023-05-25 00:41:41 +0800

    add: nlp model net id & local path

--- nlp.py
+++ nlp.py
@@ -1,21 +1,24 @@
 from flask import Blueprint, request
+import os
 nlp = Blueprint('nlp', __name__)
 
 from transformers import pipeline
 
 @nlp.route('/nlp/classification', methods=['POST'])
 def classfication():
+    # handle input
     input = request.get_json()['input']
     codes = request.get_json()['codes']
-    model_id = './Model/multilingual-MiniLMv2-L6-mnli-xnli'
-    # ------- BetterTransformer TEST: Not support DeBERTa ---------
-    # from transformers import AutoModel
-    # from optimum.bettertransformer import BetterTransformer
-    # model = AutoModel.from_pretrained(model_id)
-    # model = model.to(0)
-    # model_bt = BetterTransformer.transform(model, keep_original_model=True)
-    classfier = pipeline('zero-shot-classification',
-                         model=model_id)
+    # model
+    model_local_id = './Model/multilingual-MiniLMv2-L6-mnli-xnli'
+    model_net_id='MoritzLaurer/multilingual-MiniLMv2-L6-mnli-xnli'
+    if(os.path.exists(model_local_id)):
+        classfier = pipeline('zero-shot-classification',
+                            model=model_local_id)
+    else: 
+        classfier = pipeline('zero-shot-classification',
+                            model=model_net_id)
+    # predict
     result = classfier(input, candidate_labels=codes)
     print(result['labels'])
     return result['labels']
@@ -26,7 +29,14 @@ def keyword():
     import jieba
     input = request.get_json()['input']
     input = " ".join(jieba.cut(input))
-    model = KeyBERT(model="./Model/text2vec-base-chinese")
+    # model
+    model_local_id="./Model/paraphrase-multilingual-MiniLM-L12-v2"
+    model_net_id="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
+    if(os.path.exists(model_local_id)):
+        model = KeyBERT(model=model_local_id)
+    else:
+        model = KeyBERT(model=model_net_id)
+    # predict
     keywords = model.extract_keywords(input)
     result = []
     for keyword in keywords:
